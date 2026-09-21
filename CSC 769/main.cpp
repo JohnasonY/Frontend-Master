1af93394ac72d44a39d3368d446546851efba63c
Jax <jiaxingrong66@gmail.com>
2026-09-21 01:15:15 -0400

    CSS 769 Project 1 Done

--- CSC 769/main.cpp
+++ CSC 769/main.cpp
@@ -54,12 +54,97 @@ public:
         newEdge = new Edge{u, weight, head[v]};
         head[v] = newEdge;
     }
+
+    void findClosenessCentrality()
+    {
+        const int INF = 999999;
+
+        // Create distance matrix
+        int **dist = new int *[V];
+
+        for (int i = 0; i < V; i++)
+        {
+            dist[i] = new int[V];
+
+            for (int j = 0; j < V; j++)
+            {
+                if (i == j) // itself vertex
+                    dist[i][j] = 0;
+                else // assign to infinity for other nodes
+                    dist[i][j] = INF;
+            }
+        }
+
+        // Copy adjacency list into distance matrix
+        for (int i = 0; i < V; i++)
+        {
+            Edge *current = head[i];
+
+            while (current != nullptr)
+            {
+                // update distance if current is the adjacent vetex to i
+                dist[i][current->vertex] = current->weight;
+                current = current->next;
+            }
+        }
+
+        // Floyd-Warshall
+        for (int k = 0; k < V; k++)
+        {
+            for (int i = 0; i < V; i++)
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    if (dist[i][k] != INF &&
+                        dist[k][j] != INF &&
+                        dist[i][k] + dist[k][j] < dist[i][j])
+                    {
+                        // |i, k| + |k, j| < |i, j|
+                        dist[i][j] =
+                            dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        // Find vertex with smallest total distance
+        int centralVertex = -1;
+        int minimumSum = INF;
+
+        for (int i = 0; i < V; i++)
+        {
+            int sum = 0;
+
+            for (int j = 0; j < V; j++)
+            {
+                sum += dist[i][j];
+            }
+
+            double closeness =
+                static_cast<double>(1) / sum;
+
+            cout << "Vertex " << i
+                 << ": distance sum = " << sum
+                 << ", closeness = " << closeness
+                 << endl;
+
+            if (sum < minimumSum)
+            {
+                minimumSum = sum;
+                centralVertex = i;
+            }
+        }
+
+        cout << "\nCloseness centrality vertex: "
+             << centralVertex << endl;
+    }
 };
 
 int main()
 {
     Graph g(8);
     // example input from the project document
+    // treat vertex s as 0, vertex t as 7
     g.addEdge(0, 1, 2);
     g.addEdge(0, 2, 1);
     g.addEdge(0, 3, 2);
@@ -73,5 +158,6 @@ int main()
     g.addEdge(5, 6, 4);
     g.addEdge(5, 7, 5);
     g.addEdge(6, 7, 3);
+    g.findClosenessCentrality();
     return 0;
 }
