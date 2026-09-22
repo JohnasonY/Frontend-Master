07b16e3809a4d5915003cf6bacf6d3f4186fa29f
Jax <jiaxingrong66@gmail.com>
2026-09-22 19:44:28 -0400

    fix: code format

--- CSC 769/main.cpp
+++ CSC 769/main.cpp
@@ -95,13 +95,11 @@ public:
             {
                 for (int j = 0; j < V; j++)
                 {
-                    if (dist[i][k] != INF &&
-                        dist[k][j] != INF &&
-                        dist[i][k] + dist[k][j] < dist[i][j])
+                    if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j])
                     {
                         // |i, k| + |k, j| < |i, j|
-                        dist[i][j] =
-                            dist[i][k] + dist[k][j];
+                        // update the shortest distance between i and j if k is the intermediate vertex
+                        dist[i][j] = dist[i][k] + dist[k][j];
                     }
                 }
             }
@@ -123,10 +121,7 @@ public:
             double closeness =
                 static_cast<double>(1) / sum;
 
-            cout << "Vertex " << i
-                 << ": distance sum = " << sum
-                 << ", closeness = " << closeness
-                 << endl;
+            cout << "Vertex " << i << ": closeness = " << closeness << endl;
 
             if (sum < minimumSum)
             {
@@ -135,8 +130,7 @@ public:
             }
         }
 
-        cout << "\nCloseness centrality vertex: "
-             << centralVertex << endl;
+        cout << "\nCloseness centrality vertex: " << centralVertex << endl;
     }
 };
 
